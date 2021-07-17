3570b6691d2ed7476d83e65828422f3044137b4d
jkrumm <jokrumm@gmail.com>
2021-07-17 13:15:20 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import { FocusStyleManager } from "@blueprintjs/core";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import './index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Router} from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+require("dotenv").config();
+
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router history={history}>
+    <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+FocusStyleManager.onlyShowFocusOnTabs();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
